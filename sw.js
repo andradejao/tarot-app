b0fe7660c9c4f622ee8dd68a590dae9e8e52f80a
João Vitor Andrade <joaoandradeprime99@gmail.com>
2024-06-04 15:21:56 -0300

    Atualizando service work

--- sw.js
+++ sw.js
@@ -4,9 +4,33 @@ const cachePWA = 'cache-v1'
 // Todos os arquivos devem ser adicionados ao vetor (exceto o manifesto)
 const urlsToCache = [
   '/',
-  '/index.html',  
-  '/hal192.png',
-  '/hal512.png'
+  '/index.html',
+  '/style.css',
+  '/taro.js',  
+  '/icon192.png',
+  '/icon512.png',
+  '/img/0-louco.jpg',
+  '/img/1-mago.jpg',
+  '/img/2-sacerdotisa.jpg',
+  '/img/3-imperatriz.jpg',
+  '/img/4-imperador.jpg',
+  '/img/5-papa.jpg',
+  '/img/6-enamorados.jpg',
+  '/img/7-carro.jpg',
+  '/img/8-forca.jpg',
+  '/img/9-eremita.jpg',
+  '/img/10-fortuna.jpg',
+  '/img/11-justica.jpg',
+  '/img/12-enforcado.jpg',
+  '/img/13-morte.jpg',
+  '/img/14-temperanca.jpg',
+  '/img/15-diabo.jpg',
+  '/img/16-torre.jpg',
+  '/img/17-estrela.jpg',
+  '/img/18-lua.jpg',
+  '/img/19-sol.jpg',
+  '/img/20-julgamento.jpg',
+  '/img/21-mundo.jpg'
 ]
 
 // Instalando o Service Worker e armazenando os arquivos no cache
